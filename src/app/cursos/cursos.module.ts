ae6c92272da0913e2c47a523605bcf234b463901
cleiton <cleiton.oliveira@polisys.com.br>
2018-08-23 17:04:17 -0300

    projeto angular ate a aula 8

---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,32 @@
+import { CursosService } from './cursos.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CursosComponent } from './cursos.component';
+import { CursoDetalhesComponent } from './curso-detalhes/curso-detalhes.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+
+
+    CursosComponent,
+    CursoDetalhesComponent
+  ],
+
+  exports: [
+    CursosComponent
+
+
+  ],
+
+  providers:[
+
+    CursosService
+
+  ]
+
+
+})
+export class CursosModule { }
---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+@Component({
+    selector:'meu-primeiro-component',
+    template:`
+        <p> meu primeiro projeto component com angular 2! </p>
+    `
+})
+export class MeuPrimeiroComponent{}
